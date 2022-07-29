6d13ec65b4f216c31cf3674c26d349747cc69325
Akanksha Goyal <akankshagoyalju2000@gmail.com>
2022-07-29 14:43:26 +0530

    program

---
+++ src/tryagain/program228.java
@@ -0,0 +1,14 @@
+package tryagain;
+
+import java.util.LinkedList;
+
+public class program228 {
+    public static void main(String args[])
+    {
+        LinkedList <String> ll = new LinkedList<>();
+        ll.add("Geeks");
+        ll.add("Geeks");
+        ll.add(1,"For");
+        System.out.println(ll);
+    }
+}
