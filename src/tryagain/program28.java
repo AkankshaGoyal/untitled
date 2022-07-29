5b709ab83c1583b934a80490f94f827d56b62f01
Akanksha Goyal <akankshagoyalju2000@gmail.com>
2022-07-29 14:38:26 +0530

    Another try for github

---
+++ src/tryagain/program28.java
@@ -0,0 +1,21 @@
+package tryagain;
+
+import java.util.LinkedList;
+
+public class program28 {
+    public static void main(String args[])
+    {
+        LinkedList <String> ll = new LinkedList<>();
+        ll.add("A");
+        ll.add("B");
+        ll.addLast("C");
+        ll.addFirst("D");
+        ll.add(2,"E");
+        System.out.println(ll);
+        ll.remove("B");
+        ll.remove(3);
+        ll.removeFirst();
+        ll.removeLast();
+        System.out.println(ll);
+    }
+}
---
+++ src/tryagain/program29.java
@@ -0,0 +1,8 @@
+package tryagain;
+
+public class program29 {
+           public static void main(String args[])
+           {
+                System.out.print("Hello world");
+           }
+}
