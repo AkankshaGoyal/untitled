26e700dab08c6e615f91c04171a271ba766bf85d
Akanksha Goyal <akankshagoyalju2000@gmail.com>
2022-08-01 17:13:40 +0530

    program

--- src/tryagain/concat.java
+++ src/tryagain/concat.java
@@ -8,7 +8,16 @@ public class concat {
         String str1 = sc.nextLine();
         String str2 = sc.nextLine();
         String s3=str1.concat(str2);
+        StringBuffer sbr = new StringBuffer(s3);
+        sbr = sbr.reverse();
         //String s = str1 + str2;
-        System.out.print(s3);
+        System.out.println(sbr);
+        String rev = "";
+        char[] try1 = str2.toCharArray();
+        for(int i=try1.length-1;i>=0;i--)
+        {
+            rev = rev+ try1[i];
+        }
+        System.out.println(rev);
     }
 }
