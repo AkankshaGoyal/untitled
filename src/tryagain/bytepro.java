6555dc2f5fd34deb4e4bff0a13088b7282ed8f9e
Akanksha Goyal <akankshagoyalju2000@gmail.com>
2022-08-03 21:38:36 +0530

    program

---
+++ src/tryagain/bytepro.java
@@ -0,0 +1,13 @@
+package tryagain;
+
+public class bytepro {
+    public static void main(String args[])
+    {
+        byte a = 126;
+        System.out.print(a);
+        a++;
+        System.out.print(a);
+        a++;
+        System.out.print(a);
+    }
+}
