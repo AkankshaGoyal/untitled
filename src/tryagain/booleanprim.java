0d831466a41b6f4b71233fcc12d7dbce34958379
Akanksha Goyal <akankshagoyalju2000@gmail.com>
2022-08-03 21:35:23 +0530

    program

---
+++ src/tryagain/booleanprim.java
@@ -0,0 +1,19 @@
+package tryagain;
+
+public class booleanprim {
+
+    public static void main(String args[])
+    {
+        boolean a = false;
+        boolean b = true;
+        if(b==true)
+        {
+            System.out.println("he is geek");
+        }
+      if(a == false)
+      {
+          System.out.print("Hello geek");
+      }
+    }
+
+}
