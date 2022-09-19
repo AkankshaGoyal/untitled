e80905fd2ad3ab47216d22f46b992622626e7317
Akanksha Goyal <akankshagoyalju2000@gmail.com>
2022-09-19 13:16:00 +0530

    program

---
+++ src/tryagain/reslinear.java
@@ -0,0 +1,36 @@
+package tryagain;
+
+public class reslinear {
+
+   static void bubblesort(int arr[])
+    {
+        int n = arr.length;
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=0;j<n-i-1;j++)
+            {
+                if(arr[j]>arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+    static void printarray(int arr[])
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(" ");
+            System.out.print(arr[i]);
+        }
+    }
+
+    public static void main(String args[])
+    {
+        int arr[] = {64,34,25,12,22,11,90};
+        bubblesort(arr);
+        printarray(arr);
+    }
+
+}
